3b33ac17cafe4bb4f4227705da48db6c054ee83a
bartek3774 <bartek.stempien@poczta.onet.pl>
2020-10-04 20:23:44 +0200

    Lab1
    -get all possitive patients
    -add patient

---
+++ DP.Patients/DP.Patients/Controllers/Model/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DP.Patients.Controllers.Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime TestDate { get; set; }
+
+    }
+}
